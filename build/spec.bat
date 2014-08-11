2f39c66f0eba7067b9cae7a9c416258d94c33906
Isamu Mogi <isamu_mogi@kogasoftware.com>
2014-08-11 16:47:00 +0900

    oop.

--- build/spec.bat
+++
@@ -1,3 +0,0 @@
-set PATH="C:\Program Files (x86)\LibreOffice 4.0\program\python-core-3.3.0\bin;C:\Program Files (x86)\LibreOffice 4.0\program;%PATH%"
-"C:\Program Files (x86)\LibreOffice 4.0\program\python-core-3.3.0\bin\python" spec.py
-pause
